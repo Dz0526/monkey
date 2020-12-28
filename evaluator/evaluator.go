19f217749e8a1d62a8953db3f6dad55af503e583
Dz0526 <shining.sun0526@gmail.com>
2020-12-28 14:58:42 +0000

    eval boolean & prefix expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
     "github.com/Dz0526/monkey/object"
 )
 
+var (
+    NULL  = &object.NULL{}
+    TRUE  = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
     case *ast.Program:
@@ -13,9 +19,15 @@ func Eval(node ast.Node) object.Object {
     case *ast.ExpressionStatement:
         return Eval(node.Expression)
 
-
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
+
+    case *ast.Boolean:
+        return nativeBoolToBooleanObject(node.Value)
+
+    case *ast.PrefixExpression:
+        right := Eval(node.Right)
+        return evalPrefixExpression(node.Operator, right)
     }
 
     return nil
@@ -29,4 +41,33 @@ func evalStatements(stmts []ast.Statement) object.Object {
     }
 
     return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+    return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+    case "!":
+        return evalBangOperatorExpression(right)
+    default:
+        return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+    case TRUE:
+        return FALSE
+    case FALSE:
+        return TRUE
+    case NULL:
+        return TRUE
+    default:
+        return FALSE
+    }
 }
