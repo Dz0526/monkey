e49d442542bfd2568cb7715f6a973d0e4bb9b24f
Dz0526 <shining.sun0526@gmail.com>
2020-12-31 12:14:00 +0900

    eval return statement

--- object/object.go
+++ object/object.go
@@ -8,6 +8,7 @@ const (
     INTEGER_OBJ = "INTEGER"
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ    = "NULL"
+    RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -33,3 +34,10 @@ type NULL struct {}
 
 func (n *NULL) Type() ObjectType { return NULL_OBJ }
 func (n *NULL) Inspect() string  { return "null"}
+
+type ReturnValue struct {
+    Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
